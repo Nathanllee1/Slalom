21363a449e17e59542bb9011ca26da8a5c40202d
Nathan Lee <nathan.l.lee123@gmail.com>
2018-06-13 23:59:38 +0000

    Made the boy move

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Example()
+    {
+        Physics.gravity = new Vector3(0, -2.0F, 0);
+    }
+}
