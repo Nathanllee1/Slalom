6b52ec631c036b568c36144854903316ca05b28d
Nathan Lee <nathan.l.lee123@gmail.com>
2018-06-24 00:40:34 +0000

    Added actual turning functionality but the dude flips like hell

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,61 +8,41 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     
-    Rect LeftControlSurface = new Rect(0, 0, Screen.width / 5, Screen.height);
-    //add the right side (figure it out)
+    Rect LeftControlSurface = new Rect(0, 0, Screen.width / 2, Screen.height);
+    Rect RightControlSurface = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
+    
    
     public float thrust = 50;
     public float stabilizer = 100;
-    public Rigidbody testPush;
-    public float twistForce = 500;
+    public Rigidbody Liam;
+    public float twistSpeed = 500;
 
     private void Awake()
     {
         
     }
-    void FixedUpdate()
+    void Update()
     {
-        testPush.AddForce(transform.forward * thrust);
-        testPush.AddForce(0, stabilizer, 0);
+        //Liam.AddForce(transform.forward * thrust);
+        //Liam.AddForce(0, stabilizer, 0);
+
+        
 
         if (Input.touchCount > 0) //check amount of touches
         {
             Touch touch = Input.GetTouch(0);
             if (LeftControlSurface.Contains(touch.position))
             {
-                testPush.AddForce(-1, 4, thrust, ForceMode.Impulse);
+                transform.Rotate(Vector3.left * Time.deltaTime * twistSpeed);
+            }
+            if (RightControlSurface.Contains(touch.position))
+            {
+                //Liam.AddForce(1, 4, thrust, ForceMode.Impulse);
+                transform.Rotate(Vector3.left * Time.deltaTime * twistSpeed);
+
             }
         }
     }
 }
 
-
-/*
-using UnityEngine;
-using System.Collections;
-
-public class ExampleClass : MonoBehaviour
-{
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
-    void Update()
-    {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
-
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
-    }
-
-}
-*/
 
