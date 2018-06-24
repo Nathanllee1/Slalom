c8822a455bb208a06ab583d98d66d569328ab142
Nathan Lee <nathan.l.lee123@gmail.com>
2018-06-24 16:53:17 +0000

    Liam is more stable but the controls aren't working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,38 +8,53 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     
-    Rect LeftControlSurface = new Rect(0, 0, Screen.width / 2, Screen.height);
-    Rect RightControlSurface = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
+    
     
    
-    public float thrust = 50;
+    
     public float stabilizer = 100;
     public Rigidbody Liam;
-    public float twistSpeed = 500;
+    public float twistSpeed = 5;
+    public float testboost = 200;
+
+    private Vector3 fp;
+    private Vector3 lp;
+    private float dragDistance;
+    //private List<Vector3> touchPositions = new List<Vector3>();
+
+    private Rect LeftControlSurface;
+    private Rect RightControlSurface;
 
     private void Awake()
     {
-        
+        Liam = GetComponent<Rigidbody>();
+        Debug.Log("DebugLog");
+        Rect LeftControlSurface = new Rect(0, 0, Screen.width / 2, Screen.height);
+        Rect RightControlSurface = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
     }
     void Update()
     {
         //Liam.AddForce(transform.forward * thrust);
         //Liam.AddForce(0, stabilizer, 0);
 
-        
+        Liam.AddForce(transform.forward * testboost);
 
         if (Input.touchCount > 0) //check amount of touches
         {
+            Liam.AddTorque(-transform.right * twistSpeed);
+
             Touch touch = Input.GetTouch(0);
             if (LeftControlSurface.Contains(touch.position))
             {
-                transform.Rotate(Vector3.left * Time.deltaTime * twistSpeed);
+                Liam.AddTorque(-transform.right * twistSpeed);
+                Debug.Log("Left");
             }
             if (RightControlSurface.Contains(touch.position))
             {
                 //Liam.AddForce(1, 4, thrust, ForceMode.Impulse);
-                transform.Rotate(Vector3.left * Time.deltaTime * twistSpeed);
-
+                //transform.Rotate(Vector3.right * Time.deltaTime * twistSpeed);
+                Liam.AddTorque(transform.right * twistSpeed);
+                Debug.Log("Right");
             }
         }
     }
