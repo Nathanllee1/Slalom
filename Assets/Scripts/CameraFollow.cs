dda5ab9b5522a1a7ffb4443027c1a004f6559e11
Nathan Lee <nathan.l.lee123@gmail.com>
2018-06-28 05:38:38 +0000

    The script works!  Haha yes :) He can move around woooooo

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CameraFollow : MonoBehaviour {
-
+public class CameraFollow : MonoBehaviour
+{
     public Transform target;
-
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
     void Update()
     {
-        if (target)
-        {
-            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3 (0, 1/3, -1/3);
-        }
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,37 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    //change aspects of control
     public float stabilizer = 100;
     public Rigidbody Liam;
     public float twistSpeed = 5;
     public float testboost = 200;
 
+    /*
     private Vector3 fp;
     private Vector3 lp;
     private float dragDistance;
     //private List<Vector3> touchPositions = new List<Vector3>();
+    */
 
+    //control surfaces
     private Rect LeftControlSurface;
     private Rect RightControlSurface;
     private float ControlOffTime;
 
+
+    //swipe
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private float diffTime;
+    private float startTime;
+    private Vector2 DragDistance;
+    private float speed;
+
+    
     private void Awake()
     {
         Liam = GetComponent<Rigidbody>();
-        Debug.Log("Yes");
         LeftControlSurface = new Rect(0, 0, Screen.width / 2, Screen.height);
         RightControlSurface = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
     }
@@ -34,7 +46,12 @@ public class PlayerController : MonoBehaviour
         //Liam.AddForce(transform.forward * thrust);
         //Liam.AddForce(0, stabilizer, 0);
         //Liam.AddTorque(-transform.right * twistSpeed);
+
+
+        
         Liam.AddForce(transform.forward * testboost);
+
+        //taps
 
         if (Input.touchCount > 0) //check amount of touches
         {
@@ -43,7 +60,9 @@ public class PlayerController : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             if (LeftControlSurface.Contains(touch.position))
             {
-                Liam.AddTorque(transform.right * twistSpeed);
+                //float v = Input.GetAxis("Vertical") * twistSpeed * Time.deltaTime;
+                //Liam.AddTorque(transform.up * v);
+                Liam.AddTorque(0, -twistSpeed, 0);
                 Debug.Log("Left");
                 
             }
@@ -51,11 +70,49 @@ public class PlayerController : MonoBehaviour
             {
                 //Liam.AddForce(1, 4, thrust, ForceMode.Impulse);
                 //transform.Rotate(Vector3.right * Time.deltaTime * twistSpeed);
-                Liam.AddTorque(-transform.right * twistSpeed);
+                float v = Input.GetAxis("Vertical") * twistSpeed * Time.deltaTime;
+                Liam.AddTorque(0, twistSpeed, 0);
                 Debug.Log("Right");
             }
         }
+
+
+        //swipes
+        
+        
     }
 }
 
+/*
+foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPos = touch.position;
+                startTime = Time.time;
+            }
 
+            if(touch.phase == TouchPhase.Ended)
+            {
+                endPos = touch.position;
+                diffTime = Time.time - startTime;
+                startTime = 0;
+            }
+
+            DragDistance = (startPos - endPos).magnitude;
+
+
+            if (diffTime != 0)
+            {
+                speed = DragDistance / diffTime;
+            }
+
+            if (speed != Vector2.zero)
+            {
+                Debug.Log("startTime / diffTime = " + startTime + "/" + diffTime);
+            }
+            //Turn swipelength into force
+            
+        }
+
+*/
