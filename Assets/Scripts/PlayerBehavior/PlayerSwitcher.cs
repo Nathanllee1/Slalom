1b3ea541871190661f8bc11ba2f35dbc9ba14ab9
Nathan Lee <nathan.l.lee123@gmail.com>
2018-07-12 06:45:53 +0000

    Seperated code into different functions

---
+++ Assets/Scripts/PlayerBehavior/PlayerSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitcher : MonoBehaviour {
+
+    //Different modes that Liam can be in
+    public GameObject walking;
+    public GameObject skiing;
+
+    //GUI button
+    public Texture btnTexture;
+
+
+    //Public Variable
+    public string LiamState;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+            Debug.Log("Clicked the button with text"); //switch
+    }
+
+    //add autoswitcher for gliding
+}
